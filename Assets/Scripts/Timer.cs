b41605fa2141b76767ad6f18c6a7f98c1bdbf93e
Bailey-Caldera <bailey.caldera.k@gmail.com>
2026-05-02 20:50:10 -0700

    Added a Timer
    A timer was added where the countdown starting time is customizable and the rate it decrease is customizable.

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TMP_Text timerText;
+    [SerializeField] float remainingTime = 60;
+    [SerializeField] float decreaseTimeRate = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Count down the remain time & prevent it from going below 0
+        if (remainingTime > 0)
+            remainingTime -= Time.deltaTime * decreaseTimeRate;
+        else
+            remainingTime = 0;
+
+        // Format the time to a digital clock format
+        int minute = Mathf.FloorToInt(remainingTime / 60);
+        int second = Mathf.FloorToInt(remainingTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minute, second);
+    }
+}
